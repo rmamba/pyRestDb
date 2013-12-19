746c948350e94b92723a6c3a6e19b50d06d07007
davi0000 <dlednik@esri.com>
2013-12-19 11:34:52 +0000

    Alpha release

--- pyRestDb.py
+++ pyRestDb.py
@@ -17,43 +17,43 @@ data = { }
 
 @app.route('/')
 def list_variable():
-    #data['args'] = request.args
-    pjson = False
-    if 'pjson' in request.args:
-	pjson = True
-    return return_data(data, pjson)
+	#data['args'] = request.args
+	pjson = False
+	if 'pjson' in request.args:
+		pjson = True
+	return return_data(data, pjson)
 
 @app.route('/<variable>')
 def show_value(variable=None):
-    data['args'] = request.args
-    pjson = False
-    if 'pjson' in request.args:
-	pjson = True
-    if variable in data:
-        return return_data(val, pjson)
-    else:
-        return return_data({"response": "EMPTY"}, pjson)
+	data['args'] = request.args
+	pjson = False
+	if 'pjson' in request.args:
+		pjson = True
+	if variable in data:
+		return return_data(val, pjson)
+	else:
+		return return_data({"response": "EMPTY"}, pjson)
 
 @app.route('/<variable>/<value>')
 def set_value(variable=None, value=None):
-    data['args'] = request.args
-    pjson = False
-    if 'pjson' in request.args:
-	pjson = True
-    try:	
-	data[variable] = json.loads(value)
-	return return_data({"response": "OK"}, pjson)
-    except Exception,e:
-	return return_data({"error": str(e)}, pjson)
+	data['args'] = request.args
+	pjson = False
+	if 'pjson' in request.args:
+		pjson = True
+	try:
+		data[variable] = json.loads(value)
+		return return_data({"response": "OK"}, pjson)
+	except Exception,e:
+		return return_data({"error": str(e)}, pjson)
 
 def return_data(data, pjson=False):
-    if pjson:
-	return json.dumps(data, sort_keys = False, indent = 2, separators=(',', ': '))
-    else:
-	return json.dumps(data)
+	if pjson:
+		return json.dumps(data, sort_keys = False, indent = 2, separators=(',', ': '))
+	else:
+		return json.dumps(data)
 
 if __name__ == '__main__':
-    _host = '0.0.0.0'
-    _port = 666
-    #app.debug = True
-    app.run(host=_host, port=_port)
+	_host = '0.0.0.0'
+	_port = 666
+	#app.debug = True
+	app.run(host=_host, port=_port)
