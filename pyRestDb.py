88505158e40db4ef2ac1838ec95275f371e391b6
rmamba <rmamba@gmail.com>
2013-12-19 10:32:27 +0000

    working beta

--- pyRestDb.py
+++ pyRestDb.py
@@ -15,26 +15,37 @@ from flask import request
 app = Flask(__name__)
 data = {}
 
+@app.route('/')
+def list_variable(variable=None, value=None):
+    #data['args'] = request.args
+    pjson = False
+    return return_data(data, pjson)
+
+@app.route('/<variable>')
+def show_value(variable=None):
+    data['args'] = request.args
+    pjson = False
+    try:
+	val = data[variable]
+        return return_data(val, pjson)
+    except IndexError:
+        return return_data({"response": "EMPTY"},pjson)
+
 @app.route('/<variable>/<value>')
 def handle_variable(variable=None, value=None):
-    print request.args
+    data['args'] = request.args
     pjson = False
-    if variable == None:
-        return return_data(data, pjson)
-    else:
-        if value == None:
-            if data[variable] != None:
-                return_data(data[variable], pjson)
-            else:
-                return_data({'DOES_NOT_EXIST'},pjson)
-        else:
-            data[variable] = json.loads(value)
-            return_data({'OK'}, pjson)
+    try:
+	data[variable] = json.loads(value)
+	return return_data({"response": "OK"}, pjson)
+    except Exception,e:
+	return return_data({"error": e}, pjson)
 
 def return_data(data, pjson=False):
     return json.dumps(data)
 
 if __name__ == '__main__':
-    _host = '127.0.0.1'
+    _host = '0.0.0.0'
     _port = 666
+    app.debug = True
     app.run(host=_host, port=_port)
