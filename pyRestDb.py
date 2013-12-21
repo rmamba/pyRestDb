4bb3c2f1d7eb58a4a737ad070215fca716a02a2c
rmamba <rmamba@gmail.com>
2013-12-21 14:33:14 +0000

    Added admin control Added secret section for apps to hide data

--- pyRestDb.py
+++ pyRestDb.py
@@ -7,6 +7,7 @@ Created on 18 Dec 2013
 @author: rmamba@gmail.com
 '''
 
+import sys
 import asyncore
 import json
 
@@ -14,6 +15,8 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 data = { }
+secret = { }
+_admin = 'password'
 
 @app.route('/')
 def list_variable():
@@ -21,32 +24,100 @@ def list_variable():
 	pjson = False
 	if 'pjson' in request.args:
 		pjson = True
-	return return_data(data, pjson)
+	return return_json(data, pjson)
 
 @app.route('/<variable>')
 def show_value(variable=None):
-	data['args'] = request.args
+	#data['args'] = request.args
 	pjson = False
+	_secret = None
 	if 'pjson' in request.args:
 		pjson = True
-	if variable in data:
-		return return_data(data[variable], pjson)
+	if 'secret' in request.args:
+		_secret = request.args['secret']
+	if _secret == None:
+		if variable in data:
+			return return_json(data[variable], pjson)
 	else:
-		return return_data({"response": "EMPTY"}, pjson)
+		if _secret in secret:
+			if variable in secret[_secret]:
+				return return_json(secret[_secret][variable], pjson)
+	return return_json({"response": "EMPTY"}, pjson)
 
 @app.route('/<variable>/<value>')
 def set_value(variable=None, value=None):
-	data['args'] = request.args
+	#data['args'] = request.args
+	pjson = False
+	_secret = None
+	if 'pjson' in request.args:
+		pjson = True
+	if 'secret' in request.args:
+		_secret = request.args['secret']
+	if _secret == None:
+		data[variable] = json.loads(value)
+		return return_json({"response": "OK"}, pjson)
+	else:
+		if not _secret in secret:
+			secret[_secret] = {}
+		secret[_secret][variable] = json.loads(value)
+		return return_json({"response": "OK"}, pjson)
+
+@app.route('/admin/<password>')
+def admin(password=None):
 	pjson = False
 	if 'pjson' in request.args:
 		pjson = True
-	try:
-		data[variable] = json.loads(value)
-		return return_data({"response": "OK"}, pjson)
-	except Exception,e:
-		return return_data({"error": str(e)}, pjson)
+	if _admin != password:
+		time.sleep(1)
+		return return_json({"error": "Wrong password!"}, pjson)
+	return return_json({ "public": data, "secret": secret }, pjson)
 
-def return_data(data, pjson=False):
+@app.route('/admin/delete/<password>/<variabla>')
+def admin_delete(password=None, variable=None):
+	pjson = False
+	_secret = None
+	if 'pjson' in request.args:
+		pjson = True
+	if 'secret' in request.args:
+		_secret = request.args['secret']
+	if _admin != password:
+		time.sleep(1)
+		return return_json({"error": "Wrong password!"}, pjson)
+	
+	if _secret == None:
+		if variable in data:
+			del data[variable]
+			return return_json({"response": "OK"}, pjson)
+	else:
+		if _secret in secret:
+			if variable in secret[_secret]:
+				del secret[_secret][variable]
+				return return_json({"response": "OK"}, pjson)
+	return return_json({"response": "EMPTY"}, pjson)
+
+@app.route('/admin/purge/<password>')
+def admin_delete(password=None):
+	pjson = False
+	_secret = None
+	if 'pjson' in request.args:
+		pjson = True
+	if 'secret' in request.args:
+		_secret = request.args['secret']
+	if _admin != password:
+		time.sleep(1)
+		return return_json({"error": "Wrong password!"}, pjson)
+	
+	if _secret == None:
+		data = { }
+		secret = { }
+		return return_json({"response": "OK"}, pjson)
+	else:
+		if _secret in secret:
+			del secret[_secret]
+			return return_json({"response": "OK"}, pjson)
+	return return_json({"response": "EMPTY"}, pjson)
+
+def return_json(data, pjson=False):
 	if pjson:
 		return json.dumps(data, sort_keys = True, indent = 2, separators=(',', ': '))
 	else:
@@ -55,5 +126,20 @@ def return_data(data, pjson=False):
 if __name__ == '__main__':
 	_host = '0.0.0.0'
 	_port = 666
-	#app.debug = True
-	app.run(host=_host, port=_port)
+	try:
+		for arg in sys.argv:
+			if arg == "--debug":
+				app.debug = True
+			if arg.startswith('--host='):
+				tmp = arg.split('=')
+				_host = tmp[1]
+			if arg.startswith('--port='):
+				tmp = arg.split('=')
+				_port = tmp[1]
+			if arg.startswith('--admin='):
+				tmp = arg.split('=')
+				_admin = tmp[1]
+	
+		app.run(host=_host, port=_port)
+	except Exception,e:
+		print "Error: " + str(e)
