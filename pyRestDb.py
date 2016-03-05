2bda4a14cee98ac2b71f5ccee7c8446dcbb0d9db
dlednik <dlednik@esri.com>
2016-03-04 21:09:50 -0800

    Fixed some issues Added /generate/secret Added tests

--- pyRestDb.py
+++ pyRestDb.py
@@ -7,194 +7,216 @@ Created on 18 Dec 2013
 @author: rmamba@gmail.com
 '''
 
-import os
 import sys
-import asyncore
 import json
 import time
+import numbers
+import string
+import random
 
-#RaspberryPi: sudo apt-get install python-flask
-#OpenSuse: zypper install python-flask
-#Mac OSX: sudo easy_install Flask
+# RaspberryPi: sudo apt-get install python-flask
+# OpenSuse: zypper install python-flask
+# Mac OSX: sudo easy_install Flask
 import flask
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 
 app = Flask(__name__, static_folder='static')
-data = { }
-secret = { }
+data = {}
+secret = {}
+#Change Admin password for deployment!!!
+#Run server with --admin=newPassword
 _admin = 'password'
 
 @app.route('/')
 @app.route('/index.html')
 def index():
-	return render_template('index.html', data=data)
+    return render_template('index.html', data=data)
 
 @app.route('/static/<path:file>')
 def file(file=None):
-	if file=='favicon.ico':
-		file = 'rmamba.jpg'
-	return send_from_directory(app.static_folder, filename)
+    if file == 'favicon.ico':
+        file = 'rmamba.jpg'
+    return send_from_directory(app.static_folder, file)
 
 @app.route('/get')
 @app.route('/get/<path:variable>')
 def show_value(variable=None):
-	pjson = False
-	_secret = None
-	if 'pjson' in request.args:
-		pjson = True
-	if 'secret' in request.args:
-		_secret = request.args['secret']
-	if _secret == None:
-		val = data
-		if variable!=None:
-			path = variable.split('/')
-			for p in path:
-				if p in val:
-					val = val[p]
-				else:
-					val = None
-					break
-		if val!=None:
-			return return_json(val, pjson)
-	else:
-		if _secret in secret:
-			val = secret[_secret]
-			if variable!=None:
-				path = variable.split('/')
-				for p in path:
-					if p in val:
-						val = val[p]
-					else:
-						val = None
-						break
-			if val!=None:
-				return return_json(val, pjson)
-	return return_json({"response": "EMPTY"}, pjson)
+    pjson = False
+    _secret = None
+    if 'pjson' in request.args:
+        pjson = True
+    if 'secret' in request.args:
+        _secret = request.args['secret']
+    if _secret == None:
+        val = data
+        if variable != None:
+            path = variable.split('/')
+            for p in path:
+                if p in val:
+                    val = val[p]
+                else:
+                    val = None
+                    break
+        if val != None:
+            return return_json(val, pjson)
+    else:
+        if _secret in secret:
+            val = secret[_secret]
+            if variable != None:
+                path = variable.split('/')
+                for p in path:
+                    if p in val:
+                        val = val[p]
+                    else:
+                        val = None
+                        break
+            if val != None:
+                return return_json(val, pjson)
+    return return_json({"response": "EMPTY"}, pjson)
 
 @app.route('/post/<variable>', methods=['POST'])
 def set_value_post(variable=None):
-	if request.method != 'POST':
-		return return_json({"error": "GET is not supported for this command"}, pjson)
-	_secret = None
-	if 'secret' in request.args:
-		_secret = request.args['secret']
-	if _secret == None:
-		data[variable] = json.loads(request.data)
-	else:
-		if not _secret in secret:
-			secret[_secret] = { }
-		secret[_secret][variable] = json.loads(request.data)
-	return return_json({"response": "OK"})
+    pjson = False
+    if 'pjson' in request.args:
+        pjson = True
+    if request.method != 'POST':
+        return return_json({"error": "GET is not supported for this command"}, pjson)
+    _secret = None
+    if 'secret' in request.args:
+        _secret = request.args['secret']
+    if _secret == None:
+        data[variable] = json.loads(request.data)
+    else:
+        if not _secret in secret:
+            secret[_secret] = {}
+        secret[_secret][variable] = json.loads(request.data)
+    return return_json({"response": "OK"})
 
 @app.route('/set/<variable>/<value>')
-def set_value(variable=None, value=None):
-	pjson = False
-	_secret = None
-	if 'pjson' in request.args:
-		pjson = True
-	if 'secret' in request.args:
-		_secret = request.args['secret']
-	if _secret == None:
-		data[variable] = json.loads(value)
-		return return_json({"response": "OK"}, pjson)
-	else:
-		if not _secret in secret:
-			secret[_secret] = { }
-		secret[_secret][variable] = json.loads(value)
-		return return_json({"response": "OK"}, pjson)
+def set_string_value(variable=None, value=None):
+    if not isinstance(value, numbers.Number):
+        if value[0] != '"' and value[0] != '{':
+            value = '"' + value
+        if value[-1] != '"' and value[-1] != '}':
+            value = value + '"'
+    pjson = False
+    _secret = None
+    if 'pjson' in request.args:
+        pjson = True
+    if 'secret' in request.args:
+        _secret = request.args['secret']
+    if _secret == None:
+        data[variable] = json.loads(value)
+        return return_json({"response": "OK"}, pjson)
+    else:
+        if not _secret in secret:
+            secret[_secret] = {}
+        secret[_secret][variable] = json.loads(value)
+        return return_json({"response": "OK"}, pjson)
 
 @app.route('/admin/<password>')
 def admin(password=None):
-	pjson = False
-	if 'pjson' in request.args:
-		pjson = True
-	if _admin != password:
-		time.sleep(5)
-		return return_json({"error": "Wrong password!"}, pjson)
-		#return render_template('index.html', data=data)
-	#return return_json({ "public": data, "secret": secret }, pjson)
-	return render_template('index.html', data={ "public": data, "secret": secret })
+    pjson = False
+    if 'pjson' in request.args:
+        pjson = True
+    if _admin != password:
+        time.sleep(5)
+        return return_json({"error": "Wrong password!"}, pjson)
+    # return render_template('index.html', data=data)
+    # return return_json({ "public": data, "secret": secret }, pjson)
+    return render_template('index.html', data={"public": data, "secret": secret})
 
 @app.route('/admin/delete/<password>/<variable>')
 def admin_delete(password=None, variable=None):
-	pjson = False
-	_secret = None
-	if 'pjson' in request.args:
-		pjson = True
-	if 'secret' in request.args:
-		_secret = request.args['secret']
-	if _admin != password:
-		time.sleep(5)
-		return return_json({"error": "Wrong password!"}, pjson)
-	
-	if _secret == None:
-		if variable in data:
-			del data[variable]
-			return return_json({"response": "OK"}, pjson)
-	else:
-		if _secret in secret:
-			if variable in secret[_secret]:
-				del secret[_secret][variable]
-				return return_json({"response": "OK"}, pjson)
-	return return_json({"response": "EMPTY"}, pjson)
+    pjson = False
+    _secret = None
+    if 'pjson' in request.args:
+        pjson = True
+    if 'secret' in request.args:
+        _secret = request.args['secret']
+    if _admin != password:
+        time.sleep(5)
+        return return_json({"error": "Wrong password!"}, pjson)
+
+    if _secret == None:
+        if variable in data:
+            del data[variable]
+            return return_json({"response": "OK"}, pjson)
+    else:
+        if _secret in secret:
+            if variable in secret[_secret]:
+                del secret[_secret][variable]
+                return return_json({"response": "OK"}, pjson)
+    return return_json({"response": "EMPTY"}, pjson)
 
 @app.route('/admin/purge/<password>')
 def admin_purge(password=None):
-	pjson = False
-	_secret = None
-	if 'pjson' in request.args:
-		pjson = True
-	if 'secret' in request.args:
-		_secret = request.args['secret']
-	if _admin != password:
-		time.sleep(5)
-		return return_json({"error": "Wrong password!"}, pjson)
-	
-	if _secret == None:
-		data.clear()
-		secret.clear()
-		return return_json({"response": "OK"}, pjson)
-	else:
-		if _secret in secret:
-			del secret[_secret]
-			return return_json({"response": "OK"}, pjson)
-	return return_json({"response": "EMPTY"}, pjson)
+    pjson = False
+    _secret = None
+    if 'pjson' in request.args:
+        pjson = True
+    if 'secret' in request.args:
+        _secret = request.args['secret']
+    if _admin != password:
+        time.sleep(5)
+        return return_json({"error": "Wrong password!"}, pjson)
+
+    if _secret == None:
+        data.clear()
+        secret.clear()
+        return return_json({"response": "OK"}, pjson)
+    else:
+        if _secret in secret:
+            del secret[_secret]
+            return return_json({"response": "OK"}, pjson)
+    return return_json({"response": "EMPTY"}, pjson)
+
+@app.route('/generate/secret')
+def generate_secret(password=None):
+    pjson = False
+    if 'pjson' in request.args:
+        pjson = True
+    newSecret = id_generator()
+    while newSecret in secret:
+        newSecret = id_generator()
+    secret[newSecret] = {}
+    return return_json({"secret": newSecret}, pjson)
 
 def return_json(data, pjson=False, sortkeys=True, code=200):
-	if pjson:
-		return flask.Response(response=json.dumps(data, sort_keys=sortkeys, indent=4, separators=(',', ': ')), status=code, mimetype='application/json')
-	else:
-		return flask.Response(response=json.dumps(data, sort_keys=sortkeys), status=code, mimetype='application/json')
+    if pjson:
+        return flask.Response(response=json.dumps(data, sort_keys=sortkeys, indent=4, separators=(',', ': ')),
+                              status=code, mimetype='application/json')
+    else:
+        return flask.Response(response=json.dumps(data, sort_keys=sortkeys), status=code, mimetype='application/json')
 
 @app.errorhandler(404)
 def page_not_found(e):
     return return_json({'error': 'Page not Found!!!'}, False, True, 404)
-   
+
 @app.errorhandler(500)
 def internal_error(e):
     return return_json({'error': 'Internal server Error!!!'}, False, True, 500)
 
-if __name__ == '__main__':
-	_host = '0.0.0.0'
-	_port = 666
-	try:
-		for arg in sys.argv:
-			if arg == "--debug":
-				app.debug = True
-			if arg.startswith('--host='):
-				tmp = arg.split('=')
-				_host = tmp[1]
-			if arg.startswith('--port='):
-				tmp = arg.split('=')
-				_port = int(tmp[1])
-			if arg.startswith('--admin='):
-				tmp = arg.split('=')
-				_admin = tmp[1]
+def id_generator(size=16, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
-		pid = str(os.getpid())
-    		f = open('/var/log/pyRestDb.pid', 'w')
-    		f.write(pid)
-    		f.close()
-		app.run(host=_host, port=_port)
-	except Exception,e:
-		print "Error: " + str(e)
+if __name__ == '__main__':
+    _host = '0.0.0.0'
+    _port = 666
+    try:
+        for arg in sys.argv:
+            if arg == "--debug":
+                app.debug = True
+            if arg.startswith('--host='):
+                tmp = arg.split('=')
+                _host = tmp[1]
+            if arg.startswith('--port='):
+                tmp = arg.split('=')
+                _port = int(tmp[1])
+            if arg.startswith('--admin='):
+                tmp = arg.split('=')
+                _admin = tmp[1]
+
+        app.run(host=_host, port=_port)
+    except Exception, e:
+        print "Error: " + str(e)
---
+++ tests.py
@@ -0,0 +1,47 @@
+import unittest
+import urllib
+import urllib2
+from flask import Flask
+from flask.ext.testing import LiveServerTestCase
+import pyRestDb
+
+class testServerTestCase(LiveServerTestCase):
+
+    def create_app(self):
+        app = pyRestDb.app
+        app.config['TESTING'] = True
+        return app
+
+    def urlTest(self, url, response, encode=True):
+        if encode:
+            url = urllib.quote(url)
+        res = urllib2.urlopen(self.get_server_url()+url)
+        self.assertEqual(res.read(), response)
+        self.assertEqual(res.code, 200)
+
+    def test_server_is_up_and_running(self):
+        res = urllib2.urlopen(self.get_server_url())
+        self.assertEqual(res.code, 200)
+
+    def test_numbers_value(self):
+        self.urlTest('/set/testInt/13', '{"response": "OK"}')
+        self.urlTest('/get/testInt', '"13"')
+        self.urlTest('/set/testInt/-113', '{"response": "OK"}')
+        self.urlTest('/get/testInt', '"-113"')
+        self.urlTest('/set/testInt/-3.14', '{"response": "OK"}')
+        self.urlTest('/get/testInt', '"-3.14"')
+
+    def test_strings_value(self):
+        self.urlTest('/set/testString/stringWithNoQuotes', '{"response": "OK"}')
+        self.urlTest('/get/testString', '"stringWithNoQuotes"')
+        self.urlTest('/set/testString/"stringWithQuotes"', '{"response": "OK"}')
+        self.urlTest('/get/testString', '"stringWithQuotes"')
+
+    def test_JSON_value(self):
+        self.urlTest('/set/testJSON/{"jsonNum":3.14,"jsonStr":"pi"}', '{"response": "OK"}')
+        self.urlTest('/get/testJSON', '{"jsonNum": 3.14, "jsonStr": "pi"}')
+        self.urlTest('/get/testJSON?pjson', '{"jsonNum": 3.14, "jsonStr": "pi"}')
+        self.urlTest('/get/testJSON/?pjson', '{"jsonNum": 3.14, "jsonStr": "pi"}')
+
+if __name__ == '__main__':
+    unittest.main()
